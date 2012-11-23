c589cc7de15b2c31661f09004963fe65bca4e5f7
Richard Johnston <richard@astutelabs.com>
2012-11-23 01:37:07 +0000

    Beginning of tags
    Added Model for tags for system and join for tags related to post.
    Switched spud divider to bottom of home page.

--- Models.py
+++ Models.py
@@ -7,3 +7,10 @@ class Posts(db.Model):
  datemodified = db.DateTimeProperty(auto_now=True)
  title = db.StringProperty()
  text = db.TextProperty()
+
+class Tags(db.Model): 
+ tagname=db.StringProperty()
+ 
+class TagJoin(db.Model): 
+ post=db.ReferenceProperty(Posts,collection_name="tags_for_post")
+ bespoketag=db.ReferenceProperty(Tags)
